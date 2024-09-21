9ec067f3d00fcefbd9e01284fc347b5b661eb3dc
Mathéo DELAUNAY <dseonay@gmail.com>
2024-09-22 00:02:24 +0200

    Refactoriser le composant AnimatedLetters pour ajouter une animation d'apparition progressive des lettres

--- src/components/AnimatedLetters.jsx
+++ src/components/AnimatedLetters.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 
 const AnimatedLetters = ({ text }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect(); // Déconnecte l'observer après que l'animation ait été lancée
+        }
+      },
+      { threshold: 0.1 } // Déclenche quand 10% du texte est visible
+    );
+
+    if (textRef.current) {
+      observer.observe(textRef.current);
+    }
+
+    return () => {
+      if (textRef.current) {
+        observer.unobserve(textRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <span className="bg-secondary text-black">
+    <span ref={textRef} className="bg-secondary text-black">
       {text.split('').map((letter, index) => (
         <motion.span
           key={index}
           initial={{ opacity: 0, y: -10 }} // Position initiale
-          animate={{ opacity: 1, y: 0 }} // Position finale
+          animate={isVisible ? { opacity: 1, y: 0 } : {}} // Position finale seulement si visible
           transition={{ duration: 0.3, delay: index * 0.1 }} // Délai pour chaque lettre
         >
           {letter}
