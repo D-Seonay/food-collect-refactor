a3db00ce8413172433c70593b1310758c7bfb203
Mathéo DELAUNAY <dseonay@gmail.com>
2024-09-22 14:50:03 +0200

    Refactoriser la taille du bouton pour s'adapter aux écrans mobiles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ text, onClick }) => {
 return (
     <motion.button
         onClick={onClick}
-        className="bg-primary text-white px-6 py-2 rounded hover:bg-primary-hover transition"
+        className="bg-primary text-white px-6 py-3 md:py-2 rounded w-full hover:bg-primary-hover transition md:w-auto"
         whileTap={{ scale: 0.55 }} // Animation au clic
         transition={{ type: 'spring', stiffness: 300 }
         }
